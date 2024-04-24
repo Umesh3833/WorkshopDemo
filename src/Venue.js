8bb673ad89c4321b12a5239d599f342defaa88ff
Umesh <22m1078@iitb.ac.in>
2024-04-24 15:59:11 +0530

    Added Venue and Dates

--- src/Venue.js
+++ src/Venue.js
@@ -4,8 +4,8 @@ function Venue() {
   return (
     <section className="venue-section">
       <h2 className="venue-title">Venue and Dates</h2>
-      <p className="event-date"><strong>Date: </strong> May 22, 2024</p>
-      <p className="venue-info"><strong>Place: </strong> Victor Menezes Convention Centre (VMCC), Indian Institute of Technology (IIT), Bombay</p>
+      <p className="event-date"><strong>Date: </strong> May 24, 2024</p>
+      <p className="venue-info"><strong>Place: </strong> Collaborative Classroom, 4th floor, Girish Gaitonde Building, Department of Electrical Engineering, IIT Bombay</p>
     </section>
   );
 }
