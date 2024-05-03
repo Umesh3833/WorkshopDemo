eaabf09a50d9c0b97488016ea21e7a738bd927ac
Umesh <22m1078@iitb.ac.in>
2024-05-03 19:23:08 +0530

    updated center alignment of borkar

--- src/Venue.js
+++ src/Venue.js
@@ -4,7 +4,7 @@ function Venue() {
   return (
     <section className="venue-section">
       {/* <h2 className="venue-title">Venue and Dates</h2> */}
-      <p className="event-date"><span className="label">Date:</span> May 24, 2024</p>
+      <p className="event-date"><span className="label">Date:</span> May 24, 2024 (Friday)</p>
 <p className="venue-info"><span className="label">Venue:</span> Collaborative Classroom, 4th floor, Girish Gaitonde Building, Department of Electrical Engineering, IIT Bombay</p> </section>
   );
 }
